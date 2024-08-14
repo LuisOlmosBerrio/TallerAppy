8ad2d2cd075b07f7eb8a4af6580c51ad89e16c58
luisfer200 <luisfernandoolmosberrio56@gmail.com>
2024-08-14 00:22:08 -0500

    checkbox

--- src/app/taskcard/taskcard.component.ts
+++ src/app/taskcard/taskcard.component.ts
@@ -1,18 +1,36 @@
 import { Component, Input } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-taskcard',
   templateUrl: './taskcard.component.html',
   styleUrls: ['./taskcard.component.scss'],
 })
-export class TaskcardComponent   {
-
+export class TaskcardComponent {
   @Input() tasks: any;
 
-  constructor() {
-    
-   }
+  constructor(private alertController: AlertController) {}
+  onCheckboxChange(event: any) {
+    const isChecked = event.detail.checked;
+    console.log('Checkbox state changed. Is checked:', isChecked);
+    // Aquí puedes realizar cualquier acción que desees según el estado del checkbox
+    this.presentAlert();
+  }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'acceptable',
+      subHeader: '',
+      message: 'oki doki',
+      buttons: [
+        {
+          text: 'Okay',
+          handler: () => {
+            console.log('Okay clicked');
+          },
+        },
+      ],
+    });
 
-  
-
+    await alert.present();
+  }
 }
